daa79a5e320a26af1bdaa89bb41f193a7ecd7418
Dimpal16 <47519005+Dimpal16@users.noreply.github.com>
2019-09-01 13:02:24 +0530

    Forming a magic square
    MAGIC SQUARES A magic square is a nxn square grid (where n is the number of cells on each side) filled with distinct positive integers in the range 1,2,...,n^{2} such that each cell contains a different integer and the sum of the integers in each row, column and diagonal is equal.

---
+++ magic_square.py
@@ -0,0 +1,42 @@
+def magic_square(n):
+  magicSquare = []
+  
+  for i in range(n):
+    l=[]
+    for j in range(n):
+      l.append(0)
+    magicSquare.append(l)
+  
+  i = int(n/2)
+  j = n-1
+  
+  num=n*n
+  count=1
+  
+  while(count<=num):
+    if(i==-1 and j==n):
+      i=0
+      j=n-2
+    else:
+      if(i<0):
+        i=n-1
+      if(j==n):
+        j=0
+      
+    if(magicSquare[i][j]!=0):
+      j=j-2
+      i=i+1
+      continue
+    else:
+      magicSquare[i][j]=count
+      count=count+1
+    
+    i=i-1
+    j=j+1
+  for i in range(n):
+    for j in range(n):
+      print(magicSquare[i][j], end=' ')
+    print()
+  
+size = int(input("Enter the size of magic square: "))
+magic_square(size)
